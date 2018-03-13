e5c77572437083a7eb61c231b03f2b96f21dd14d
Simon Bashir <admin@itproserve.com>
2018-03-13 20:12:43 +0000

    modal update

--- views/partials/modal.php
+++ views/partials/modal.php
@@ -3,26 +3,26 @@
     <i class="hs-icon hs-icon-close"></i>
   </button>
 
-  <h4 class="g-mb-20">What should you expect from us</h4>
+  <h4 class="g-mb-20">What should you expect from us?</h4>
 
-  <h5>Not only your new website will have the look and feel of a modern, professional website but you will also have ALL these features:</h5>
+  <h5>Not only your new website will have the "look and feel" of a modern, professional website but you will also have all of the following features:</h5>
 
   <ol>
-    <li>Eye-catching design: enjoy a new elegant and responsive user-interface. A layout that will attract more visitors to your website. All our beautiful layouts are based on the latest version of Bootstrap.</li>
+    <li>Eye-catching design: enjoy a new elegant and responsive user-interface. A layout that will attract more visitors to your website. All our beautiful layouts are based on the latest stable version of Bootstrap.</li>
     <li>Modern Tools: we use some of the best development tools of today:
       <ul>
         <li>Laravel (the most popular MVC framework for PHP)</li>
-        <li>Vue (one of the most popular and a progressive JavaScript framework)</li>
-        <li>HTML, CSS, Webpack, Git, Node.js and NPM. In addition to some popular open source plug-ins and libraries</li>
+        <li>Vue.js (one of the most popular and a progressive JavaScript framework)</li>
+        <li>HTML, CSS, Webpack, Git, Node.js, and NPM. In addition to various popular open source plug-ins and libraries</li>
       </ul>
     </li>
     <li>Powerful and extensive Admin dashboard to manage the overall functionality of the site.</li>
-    <li>Customer dashboard with details on all previous purchases, reviews, favorites, and other communication with the management team.</li>
-    <li>We are Agile: as soon as you have a new idea or request a new customized feature, we can work together to design, develop, test, and deploy to your hosting account any new feature in a reasonably short time (we adopt incremental approach and the Agile method for project development)</li>
+    <li>Customer profile with details on all previous purchases, reviews, favorites, and other communications with the management team.</li>
+    <li>Agile project development: as soon as you have a new idea or request a new customized feature, we can work together to design, develop, test, and deploy to your hosting account the completed new feature(s) in short cycles (we adopt incremental approach and the Scrum for project development)</li>
     <li>Improved performance: our code is optimized for fast loading and easy navigation.</li>
     <li>Improved Security: with robust secure payment gateway from some of the leaders in the industry (such as Stripe), you can have the peace of mind that your customers data will stay secure and protected.</li>
     <li>Reliable customer support: all your questions will be answered within 24 hours. You will even enjoy free training and technical support. Since we are local, we will always be close to your business when you need us.</li>
-    <li>Enhancement and updates are always FREE with your monthly subscription.</li>
+    <li>Future enhancements and updates are always FREE with your current subscription.</li>
   </ol>
-  
+
 </div>
